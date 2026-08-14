a4d60c356ff2a25100cb67f3af13fafd8d814ac7
montasir132 <montasiralam132@gmail.com>
2026-08-14 21:53:24 +0600

    Primitive non-primitive syntax ts

---
+++ Primitive_non-primitive_syntax.js
@@ -0,0 +1,32 @@
+"use strict";
+let userName = "Hello World!";
+userName = "rah";
+userName = 'dgf';
+console.log(userName);
+//variable
+const studentsRoll = 246471;
+console.log(studentsRoll);
+// array
+let arr = [10, 20, 30, 40, 50];
+// object
+let obj = {
+    name: "rahiem",
+    Roll: 74,
+    isStudent: true,
+    // class : 7
+};
+// tople
+let tople = ["rsgim", 41];
+// function
+function add(a, b) {
+    return a + b;
+}
+console.log(add(7, 74));
+// Arrow function
+let username = (a, b) => {
+    return a + b;
+};
+// no return function
+let schoolName = (a, b) => {
+    console.log(a + b); // no return
+};
